9942ab8109d02d6fe64b06dbb6a2c4fbcb9e7be3
s.kittaka <ninato.ksagmkhy@gmail.com>
2024-03-16 22:46:22 +0900

    通常の加算

--- SIMD/SIMD.cpp
+++ SIMD/SIMD.cpp
@@ -2,10 +2,44 @@
 //
 
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+void printClock(clock_t elapsedTime)
+{
+    cout << "elapsed time is " << elapsedTime << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    clock_t start, end;
+
+    /* C */
+    const size_t size = 2000 * 2000;
+    unsigned short a[size] = { 0 };
+    unsigned short b[size] = { 0 };
+    int c[size] = { 0 };
+    int i;
+
+    for (i = 0; i < size; i++) {
+        a[i] = 1;
+        b[i] = 2;
+    }
+
+    start = clock();
+    for (i = 0; i < size; i++) {
+        c[i] = a[i] + b[i];
+    }
+    end = clock();
+
+    cout << "a[0]=" << a[0] << "  a[size - 1]=" << a[size - 1] << endl;
+    cout << "b[0]=" << b[0] << "  b[size - 1]=" << b[size - 1] << endl;
+    cout << "c[0]=" << c[0] << "  c[size - 1]=" << c[size - 1] << endl;
+    printClock(end - start);
+
+
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
